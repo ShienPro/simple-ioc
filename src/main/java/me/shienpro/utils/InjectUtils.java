c368676bb09dcad7a22350f4031ddaf6362817ce
ShienPro <805952135@qq.com>
2019-04-01 18:12:50 +0800

    Fix setter injection error

--- src/main/java/me/shienpro/utils/InjectUtils.java
+++ src/main/java/me/shienpro/utils/InjectUtils.java
@@ -5,6 +5,7 @@ import me.shienpro.excepiton.InjectException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class InjectUtils {
 
@@ -26,15 +27,22 @@ public class InjectUtils {
     public static void injectBySetter(Field field, Object target, Object property) {
         if (field == null) throw new InjectException(new NullPointerException("field can not be null"));
 
-        String setterName = isBoolean(field) ? "is" : "get" +
+        String setterName = "set" +
                 field.getName().substring(0, 1).toUpperCase() +
                 field.getName().substring(1);
-        try {
-            Method setter = target.getClass().getDeclaredMethod(setterName);
-            setter.invoke(target, property);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            throw new InjectException(e);
-        }
+
+        Method[] methods = target.getClass().getDeclaredMethods();
+        Arrays.stream(methods)
+                .filter(method -> method.getName().equals(setterName) &&
+                        method.getParameterTypes().length == 1 &&
+                        method.getParameterTypes()[0].isInstance(property))
+                .forEach(method -> {
+                    try {
+                        method.invoke(target, property);
+                    } catch (InvocationTargetException | IllegalAccessException e) {
+                        throw new InjectException(e);
+                    }
+                });
     }
 
     private static boolean isBoolean(Field field) {
