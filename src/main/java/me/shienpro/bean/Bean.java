1662a981856c7dd9267253b601fd6db5a6564b67
ShienPro <805952135@qq.com>
2019-10-08 16:40:28 +0800

    Remove duplicate default values ​​for scope

--- src/main/java/me/shienpro/bean/Bean.java
+++ src/main/java/me/shienpro/bean/Bean.java
@@ -11,7 +11,7 @@ public class Bean {
     public final static int SINGLETON = 0;
     public final static int PROTOTYPE = 1;
 
-    private int scope = SINGLETON;
+    private int scope;
     private String beanName;
     private Class<?> beanClass;
     private List<ConstructorArg> constructorArgs;
